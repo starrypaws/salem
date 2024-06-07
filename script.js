d20da8b844048ecc5dae10d9529a3affe8bc5325
Salem <stxrrypaws@proton.me>
2024-06-07 10:07:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const dropdownToggles = document.querySelectorAll('.dropdown-toggle');
-  const dropdownContents = document.querySelectorAll('.dropdown-content');
-  const menuBtn = document.querySelector('.menu_btn'); // Ensure this selector matches your actual menu button
+document.addEventListener("DOMContentLoaded", function () {
+  const dropdownToggles = document.querySelectorAll(".dropdown-toggle");
+  const secret = document.querySelectorAll(".secret")
+  
+   secret.forEach((toggle) => {
+    toggle.addEventListener("click", function (event) {
+      const h2 = document.querySelector("h2")
+      const wrapper = document.querySelector(".wrapper")
+      if (h2.style.display === "none") {
+      h2.style.display = "block";
+      wrapper.style.display = "none";
+      } else {
+        h2.style.display = "none";
+        wrapper.style.display = "block";
+      }
+      event.preventDefault();
+    })
+  })
 
   dropdownToggles.forEach((toggle) => {
-    toggle.addEventListener('click', function(event) {
+    toggle.addEventListener("click", function (event) {
       this.classList.toggle("active");
       event.preventDefault();
-      const targetDropdownContent = document.querySelector(`[data-target="${this.dataset.dropdown}"]`);
-      if (targetDropdownContent.style.display === 'block') {
-        targetDropdownContent.style.display = 'none';
+      const targetDropdownContent = document.querySelector(
+        `[data-target="${this.dataset.dropdown}"]`
+      );
+      if (targetDropdownContent.style.display === "block") {
+        targetDropdownContent.style.display = "none";
       } else {
-        targetDropdownContent.style.display = 'block';
+        targetDropdownContent.style.display = "block";
       }
     });
   });
 });
-
